25bebf6bf01b3b9bb1bd6a64224295414353530a
jendevane <87095302+jendevane@users.noreply.github.com>
2021-11-06 21:13:58 -0500

    added port

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
