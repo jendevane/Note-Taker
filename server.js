6799debc62f3e81c898e6b5fb15854aa205fbe61
jendevane <87095302+jendevane@users.noreply.github.com>
2021-11-06 21:23:27 -0500

    added html routes

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 
 app.use(express.static('public'));
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use('/', htmlRoutes);
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 });
+
